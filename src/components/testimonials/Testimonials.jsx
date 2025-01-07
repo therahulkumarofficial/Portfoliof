f5d7ded10feea41075ed7134c74e5aff84298cc0
Shivam Saurabh <133574790+shivamsaurabh25@users.noreply.github.com>
2025-01-07 15:46:15 +0530

    Update Testimonials.jsx

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,31 +1,13 @@
 import React from 'react';
 import './Testimonials.css';
-/* import Image3 from '../../assets/avatar-3.svg'
-import Image4 from '../../assets/avatar-4.svg' */
-
 import { Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 import 'swiper/css';
 import 'swiper/css/pagination';
 
 const Testimonials = () => {
 
 	const data = [
-		/* {
-			id: 1,
-			image: Image4,
-			title: "Kerren Ortlepp",
-			subtitle: "Founder @ Entrostat",
-			comment: "Greg worked at Entrostat as a full-stack developer. He was excellent and had a real eye for design. If there was ever a time where we had a complicated frontend or needed to know what the best CSS approach was for something - Greg was our goto man!",
-		}, */
-		/* {
-			id: 2,
-			image: Image3,
-			title: "Thapelo Kekana",
-			subtitle: "Frontend Developer @ FNB",
-			comment: "I enjoy working with the theme and learn so much. You guys make the process fun and interesting. Good luck! 👍",
-		}, */
 	];
 
 
