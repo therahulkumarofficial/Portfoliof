39fa0a09be623268ff8b9a91fa61d5a0e176d149
Shivam Saurabh <133574790+shivamsaurabh25@users.noreply.github.com>
2025-01-07 15:38:02 +0530

    Update Blog.jsx

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -8,22 +8,6 @@ const Blog = () => {
         <section className="blog container section" id="blog">
             <h2 className="section__title">Latest Posts</h2>
             <p>No bolgs yet</p>
-            {/* <div className="blog__container grid">
-                <div className="blog__card">
-                    <div className="blog__thumb">
-                        <a href="#"><span className="blog__category">Example</span></a>
-                        <a href="#"><img src={Image1} alt="" className='blog__img' /></a>
-                    </div>
-                    <div className="blog__details">
-                        <h3 className="blog__title">Example Blog</h3>
-                        <div className="blog__meta">
-                            <span>01 January, 2025</span>
-                            <span className="blog__dot">.</span>
-                            <span>Shivam</span>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
         </section>
     )
 }
